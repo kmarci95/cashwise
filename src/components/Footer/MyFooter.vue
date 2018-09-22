a70efa149a11a905f1909094023ab9b3860a9e30
qeuio <petrocselli94@gmail.com>
2018-09-22 20:17:55 +0200

    added footer

---
+++ src/components/Footer/MyFooter.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="myfooter">
+    <div class="container row">
+      <div class="col-md-12">{{ new Date().getFullYear() }} - Cashwise</div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'MyFooter'
+  }
+</script>
+
--- src/views/DashboardPage.vue
+++ src/views/DashboardPage.vue
@@ -2,16 +2,19 @@
   <div>
     <navbar></navbar>
     <h1>Dashboard Page</h1>
+    <myfooter></myfooter>
   </div>
 </template>
 
 <script>
   import Navbar from "../components/Navbar/Navbar.vue";
+  import MyFooter from "../components/Footer/MyFooter.vue";
+  import Home from "./Home.vue";
   import axios from 'axios';
 
   export default {
     name: "DashboardPage",
-    components: {Navbar},
+    components: {Navbar, MyFooter},
   }
 </script>
 
